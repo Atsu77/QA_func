8f04dd79b27093bf6a1a0a6cddc14f78ddba0098
Atsu <kamnknon@gmail.com>
2021-06-13 15:03:21 +0900

    save_tagインスタンスメソッドを作成

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,19 @@ class Question < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100}
   validates :body, presence: true, length: { maximum: 500}
   validates :answer, presence: true, length: { maximum: 100}
+  
+  def save_tag(sent_tags)
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    old_tags = current_tags - sent_tags
+    new_tags = sent_tags - current_tags
+    
+    old_tags.each do |old|
+      self.tags.delete Tag.find_by(tag_name: old)
+    end
+
+    new_tags.each do |new|
+      new_question_tag = Tag.find_or_create_by(tag_name: new)
+      self.question_tags << new_question_tag
+    end
+  end
 end
