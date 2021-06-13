ba5c7ed461284a6a8fc2be550e27638d99c189ee
Atsu <kamnknon@gmail.com>
2021-06-13 11:54:50 +0900

    userモデルのseedファイルを作成

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'bootstrap'
 gem 'jquery-rails'
 gem 'devise'
 gem 'devise-i18n'
+gem 'faker'
+gem 'gimei'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.create!(
+	name: 'admin_user', 	
+	email: 'kamnknon@gmail.com',
+	password: 'foobar'
+)
+
+50.times do
+  sample_name = Gimei.kanji
+  sample_email = Faker::Internet.email
+  User.create!(
+    name: sample_name,
+    email: sample_email,
+    password: 'foobar'
+  )
+end
