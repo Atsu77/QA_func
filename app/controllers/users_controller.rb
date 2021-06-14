25523669898db77aad0e64c4a71c064516e3c6fb
Atsu <kamnknon@gmail.com>
2021-06-14 12:02:56 +0900

    user詳細ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   def index
     @users = User.order(id: 'DESC').page(params[:page]).per(15)
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @questions = @user.questions.order(id: 'DESC').page(params[:page]).per(5)
+  end
 end
