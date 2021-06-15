15b215dc571a4df7367d98e0151d9c5b0d616ddb
Atsu <kamnknon@gmail.com>
2021-06-15 19:06:44 +0900

    ユーザー情報編集ページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,22 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     @questions = @user.questions.order(id: 'DESC').page(params[:page]).per(5)
   end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'ユーザー情報を変更しました'
+    else
+      render action: :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
