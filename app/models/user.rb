f9ab773569189214d10388724e5ac07fcf529c47
Atsu <kamnknon@gmail.com>
2021-07-04 15:13:39 +0900

    ゲストログイン機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
   validates :name, presence: true
   
   has_many :questions, dependent: :destroy
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.name = 'ゲストユーザー'
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
